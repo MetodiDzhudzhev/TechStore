38c0c42256af7df022ed4d703cbd1b3009d43c23
MetodiDzhudzhev <metodidjudjev@gmail.com>
2025-07-28 02:03:11 +0300

    Implement dynamic Category dropdown in navbar.

---
+++ TechStore/ViewComponents/CategoryDropdownViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechStore.Services.Core.Interfaces;
+using TechStore.Web.ViewModels.Product;
+
+namespace TechStore.Web.ViewComponents
+{
+    public class CategoryDropdownViewComponent : ViewComponent
+    {
+        private readonly ICategoryService categoryService;
+
+        public CategoryDropdownViewComponent(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            IEnumerable<AddProductCategoryDropDownModel> categories =
+                await this.categoryService.GetCategoriesDropDownDataAsync();
+
+            return View(categories);
+        }
+    }
+}
