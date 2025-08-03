9948e50c372b8d4a8752fab3038096fbec297978
MetodiDzhudzhev <metodidjudjev@gmail.com>
2025-08-03 23:13:22 +0300

    Introduce ControlPanel area.

---
+++ TechStore/Areas/ControlPanel/Controllers/BaseControlPanelController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace TechStore.Web.Areas.ControlPanel.Controllers
+{
+    [Area("ControlPanel")]
+    [Authorize]
+    public abstract class BaseControlPanelController : Controller
+    {
+        private bool IsUserAuthenticated()
+        {
+            return this.User.Identity?.IsAuthenticated ?? false;
+        }
+
+        protected string? GetUserId()
+        {
+            string? userId = null;
+            bool isAuthenticated = this.IsUserAuthenticated();
+
+            if (isAuthenticated)
+            {
+                userId = this.User
+                            .FindFirstValue(ClaimTypes.NameIdentifier);
+            }
+            return userId;
+        }
+    }
+}
---
+++ TechStore/Areas/ControlPanel/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechStore.Web.Areas.ControlPanel.Controllers
+{
+    public class HomeController : BaseControlPanelController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
