33608029660ed50a7672a4f09291e75747be2039
MetodiDzhudzhev <metodidjudjev@gmail.com>
2026-01-05 01:20:29 +0200

    Add Cart ViewModels

---
+++ TechStore.Web.ViewModels/Cart/CartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using TechStore.Web.ViewModels.CartProduct;
+
+namespace TechStore.Web.ViewModels.Cart
+{
+    public class CartViewModel
+    {
+        public IEnumerable<CartProductDetailsViewModel> Products { get; set; } = new List<CartProductDetailsViewModel>();
+
+        public decimal TotalPrice => Products.Sum(p => p.Price * p.Quantity);
+    }
+}
---
+++ TechStore.Web.ViewModels/CartProduct/CartProductDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace TechStore.Web.ViewModels.CartProduct
+{
+    public class CartProductDetailsViewModel
+    {
+        public Guid ProductId { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string? ImageUrl { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
