0fb7743a0eb7fd414daf5f55868beb4a47235545
MetodiDzhudzhev <metodidjudjev@gmail.com>
2026-02-02 02:43:25 +0200

    Fix access handling in Control Panel HomeController.

--- TechStore/Areas/ControlPanel/Controllers/HomeController.cs
+++ TechStore/Areas/ControlPanel/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TechStore.Web.Areas.ControlPanel.Controllers
 {
+    [Area("ControlPanel")]
+    [Authorize(Roles = "Admin,Manager")]
     public class HomeController : BaseControlPanelController
     {
         public IActionResult Index()
