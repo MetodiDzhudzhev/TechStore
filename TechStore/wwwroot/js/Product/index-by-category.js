442c9d2ffa4c0a50d5e766710ea6669da6be8f46
MetodiDzhudzhev <metodidjudjev@gmail.com>
2026-03-01 01:04:15 +0200

    Refactor IndexByCategory page with custom styling.

---
+++ TechStore/wwwroot/js/Product/index-by-category.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+
+    const autoSubmitSelect = document.querySelector("[data-auto-submit]");
+
+    if (autoSubmitSelect) {
+        autoSubmitSelect.addEventListener("change", function () {
+            this.form.submit();
+        });
+    }
+
+});
