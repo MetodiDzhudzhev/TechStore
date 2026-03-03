b3fecc3e384e8549b4e62070010966d351efddcc
MetodiDzhudzhev <metodidjudjev@gmail.com>
2026-03-04 00:16:29 +0200

    Refactor Manage orders page.

---
+++ TechStore/wwwroot/js/ControlPanel/Order/manage.js
@@ -0,0 +1,15 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+
+    const toggles = document.querySelectorAll(".js-toggle-details");
+
+    toggles.forEach(btn => {
+        btn.addEventListener("click", function () {
+
+            const targetId = this.dataset.target;
+            const details = document.getElementById(targetId);
+
+            details.classList.toggle("is-open");
+        });
+    });
+
+});
