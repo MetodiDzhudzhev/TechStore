bad8d18ec2f48e4c25ba560e1da3736cee89af61
MetodiDzhudzhev <metodidjudjev@gmail.com>
2026-03-06 00:55:55 +0200

    Refactor Manage reviews page.

---
+++ TechStore/wwwroot/js/ControlPanel/Review/manage.js
@@ -0,0 +1,23 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+
+    const buttons = document.querySelectorAll(".js-review-toggle");
+
+    buttons.forEach(btn => {
+
+        const targetId = btn.dataset.target;
+        const target = document.getElementById(targetId);
+        const text = btn.querySelector(".btn-text");
+
+        btn.addEventListener("click", () => {
+
+            target.classList.toggle("is-open");
+
+            if (target.classList.contains("is-open")) {
+                text.textContent = "Hide details";
+            }
+            else {
+                text.textContent = "Details";
+            }
+        });
+    });
+});
