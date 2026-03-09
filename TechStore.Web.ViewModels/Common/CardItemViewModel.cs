c0c12153208b02af13ac203715571b6299433742
MetodiDzhudzhev <metodidjudjev@gmail.com>
2026-03-09 23:52:23 +0200

    Introduce reusable CardGrid ViewComponent for management pages.

---
+++ TechStore.Web.ViewModels/Common/CardItemViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TechStore.Web.ViewModels.Common
+{
+    public class CardItemViewModel
+    {
+        public string Name { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+        public string DetailsUrl { get; set; } = null!;
+        public string EditUrl { get; set; } = null!;
+        public string DeleteUrl { get; set; } = null!;
+        public bool ShowName { get; set; }
+    }
+}
---
+++ TechStore/ViewComponents/CardGridViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechStore.Web.ViewModels.Common;
+
+namespace TechStore.Web.ViewComponents
+{
+    public class CardGridViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(IEnumerable<CardItemViewModel> items)
+        {
+            return View(items);
+        }
+    }
+}
